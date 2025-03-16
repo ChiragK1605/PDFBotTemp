4ef5588202a2552dcfff61b110e4dee80e6a2e2e
ChiragK1605 <chiragkaushik1601@gmail.com>
2025-03-16 21:17:37 +0530

    app.py

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ from langchain.chains import SequentialChain
 from langchain.chains import ConversationChain
 from langchain.chains.question_answering import load_qa_chain
 
-
-working_dir=os.path.dirname(os.path.abspath(__file__))
 with st.sidebar:
     st.title("Chat Bot ")
     st.markdown('''
@@ -45,15 +43,10 @@ def main():
             
     #st.write(chunks)
     
-    store_name = pdf.name[:-4]
-    if os.path.exists(f"{store_name}.pkl"):
-        with open(f"{store_name}.pkl","rb") as f:
-            VectorStore = pickle.load(f)
-    else:
+    
         embeddings = HuggingFaceEmbeddings()
         VectorStore=FAISS.from_texts(chunks, embedding=embeddings)
-        with open(f"{store_name}.pkl","wb") as f:
-            pickle.dump(VectorStore,f)
+        
             #st.write(text)
 
     query=st.text_input("Ask any Question:")
